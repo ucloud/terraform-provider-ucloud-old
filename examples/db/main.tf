e43f542b9e747f0f7aed07ac0c61f9ced2946c7b
mingsheng.su <mingsheng.su@ucloud.cn>
2018-11-27 19:07:42 +0800

    add db example

--- examples/db/main.tf
+++ examples/db/main.tf
@@ -3,54 +3,36 @@ provider "ucloud" {
   region = "${var.region}"
 }
 
-resource "ucloud_db_instance" "master" {
-  availability_zone    = "cn-bj2-03"
-  name                 = ""
-  engine               = "mongodb"
-  engine_version       = "3.2"
-  username             = ""
-  password             = ""
-  port                 = 3306
-  cpu                  = 1
-  memory               = 1
-  instance_type        = "primary"
-  instance_duration    = 1
-  instance_charge_type = "Month"
-  instance_storage     = 20
+# Query availability zone
+data "ucloud_zones" "default" {}
 
-  # vpc
-  vpc_id    = ""
-  subnet_id = ""
-
-  # param group
-  param_group_id = ""
-
-  # MySQL master/slave
-  is_slave = false
+# Create parameter group
+data "ucloud_db_parameter_groups" "default" {
+  availability_zone = "${data.ucloud_zones.default.zones.0.id}"
+  region_flag       = "false"
+  engine            = "mysql"
+  engine_version    = "5.7"
 }
 
-resource "ucloud_db_instance" "slave" {
-  engine         = "mysql"
-  engine_version = "5.7"
+# Create database instance
+resource "ucloud_db_instance" "master" {
+  availability_zone  = "${data.ucloud_zones.default.zones.0.id}"
+  name               = "tf-example-db-instance"
+  instance_storage   = 20
+  instance_type      = "mysql-ha-1"
+  engine             = "mysql"
+  engine_version     = "5.7"
+  password           = "${var.db_password}"
+  parameter_group_id = "${data.ucloud_db_parameter_groups.default.parameter_groups.0.id}"
+}
 
-  availability_zone    = "cn-bj2-03"
-  name                 = ""
-  engine               = "mongodb"
-  engine_version       = "3.2"
-  username             = ""
-  password             = ""
-  port                 = 3306
-  cpu                  = 1
-  memory               = 1
-  instance_type        = "primary"
-  instance_duration    = 1
-  instance_charge_type = "Month"
-  instance_storage     = 20
-
-  # vpc
-  vpc_id    = ""
-  subnet_id = ""
-
-  is_slave  = true
-  master_id = "${ucloud_db_instance.master.id}"
+# Create database slave
+resource "ucloud_db_slave" "slave" {
+  availability_zone  = "${data.ucloud_zones.default.zones.0.id}"
+  name               = "f-example-db-slave"
+  instance_storage   = 20
+  instance_type      = "mysql-basic-1"
+  password           = "${var.db_password}"
+  parameter_group_id = "${data.ucloud_db_parameter_groups.default.parameter_groups.0.id}"
+  master_id          = "${ucloud_db_instance.master.id}"
 }
